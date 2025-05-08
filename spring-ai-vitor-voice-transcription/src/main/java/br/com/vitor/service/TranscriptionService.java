221039b9364b2cb7cff8e91b6872fc8db0d1e9cd
VitorCostaVianna <vcvianna@sga.pucmminas.br>
2025-05-08 18:19:57 -0300

    transcription service

---
+++ spring-ai-vitor-voice-transcription/src/main/java/br/com/vitor/service/TranscriptionService.java
@@ -0,0 +1,50 @@
+package br.com.vitor.service;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class TranscriptionService {
+    
+    private final OpenAiAudioTranscriptionModel transcriptionModel;
+    
+    public TranscriptionService(@Value("${spring.ai.openai.api-key}") String apiKey) {
+        OpenAiAudioApi openAiAudioApi = OpenAiAudioApi.builder()
+                .apiKey(apiKey)    
+                .build();
+        this.transcriptionModel = new OpenAiAudioTranscriptionModel(openAiAudioApi);
+    }
+
+    public String transcribeAudio(MultipartFile file) throws IOException{
+        
+        File tempFile = File.createTempFile("audio", "mp3");
+        file.transferTo(tempFile);
+
+        OpenAiAudioTranscriptionOptions transcriptionOptions =
+                OpenAiAudioTranscriptionOptions.builder()
+                    .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.TEXT)
+                        .language("pt")
+                        .temperature(0f) // nada criativo
+                        .build();
+        FileSystemResource audioFile = new FileSystemResource(tempFile);
+
+        AudioTranscriptionPrompt transcriptionRequest = new AudioTranscriptionPrompt(audioFile, transcriptionOptions);
+
+        AudioTranscriptionResponse response = transcriptionModel.call(transcriptionRequest);
+        
+        tempFile.delete();
+        
+        return response.getResult().getOutput();
+    }
+
+}
