6ae5c27341fd1258ca2f68e094a80dba923babd1
VitorCostaVianna <vcvianna@sga.pucmminas.br>
2025-05-08 17:45:30 -0300

    first commit

---
+++ spring-ai-vitor-voice-transcription/src/main/java/br/com/vitor/config/WebConfig.java
@@ -0,0 +1,26 @@
+package br.com.vitor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer(){
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:8080/" , "http://localhost:3000","http://localhost:5173")
+                    .allowedMethods("GET", "POST" , "DELETE" , "PUT", "OPTIONS") // options for react or aangular 
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+            }
+        };
+    }
+
+}
